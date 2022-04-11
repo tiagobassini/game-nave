c873369435455cd423e14a6b6a21a800471f4a28
tiagobassini <tiagobassini@gmail.com>
2022-04-11 13:36:32 -0300

    controle do player

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # game-nave
 Game de nave utilizando html5, CSS3 e JS
+
+Link para acesso ao jogo:
+
+< https://tiagobassini.github.io/game-nave/ >
--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+
 function start() {
 
     $("#inicio").hide();
@@ -12,25 +14,68 @@ function start() {
     //Principais variaveis do jogo
     var jogo = {}
 
-    //Game Loop
+    var TECLA = {
+        W: 87,
+        S: 83,
+        D: 68
+    }
 
+    jogo.pressionou = [];
+
+    //Verifica se o usuário pressionou alguma tecla	
+    $(document).keydown(function (e) {
+        jogo.pressionou[e.which] = true;
+    });
+
+
+    $(document).keyup(function (e) {
+        jogo.pressionou[e.which] = false;
+    });
+
+    //Iniciando o game Loop
     jogo.timer = setInterval(loop, 30);
-    
+
+
+
+    //Função de loop do jogo
+    function loop() {
+
+        movefundo();
+        movejogador();
+
+    }
+
+    //Função que movimenta o fundo do jogo
+    function movefundo() {
+
+        esquerda = parseInt($("#fundoGame").css("background-position"));
+        $("#fundoGame").css("background-position", esquerda - 2);
+
+    }
+
+
+    //Função para movimentar o Jogador 
+    function movejogador() {
+
+        if (jogo.pressionou[TECLA.W]) {
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top", topo - 10);
+
+        }
+
+        if (jogo.pressionou[TECLA.S]) {
+
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top", topo + 10);
+        }
+
+        if (jogo.pressionou[TECLA.D]) {
+
+            //Chama função Disparo	
+        }
+
+    }
+
 }
 
-//Função de loop do jogo
-function loop() {
-
-    movefundo();
-
-}
-
-//Função que movimenta o fundo do jogo
-
-function movefundo() {
-
-    esquerda = parseInt($("#fundoGame").css("background-position"));
-    $("#fundoGame").css("background-position", esquerda - 2);
-
-} // fim da função movefundo()
 
